d533c5de5f12d61fe14abca54221dd03620ae43c
Romanchu <maro.pereyra@lumno.etec.um.edu.ar>
2024-08-22 09:36:10 -0300

    css

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 import './body.css';
 import paduno from '../../multimedia/pad1.webp';
 
-
-
 export default function Body() {
   const handleCompraClick = () => {
     alert('Producto añadido'); 
@@ -14,21 +12,28 @@ export default function Body() {
     <>
       <div>
         <h1 className="palabra1">A tu alcance cualquier tipo de pad...</h1>
-        <img src={paduno} alt="imgpad" className="pad1" />
-        <div className="comprar">
-          <button className="boton1" onClick={handleCompraClick}>
-            <Link to="/carrito" className="boton">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              Añadir producto
-            </Link>
-          </button>
-        </div>
         <h1 className="palabra2">BE</h1>
         <h1 className="palabra3">MUST</h1>
       </div>
+      
+      {/* Sección de Productos */}
+      <section className="productos">
+        <h2 className="productos-titulo">Productos</h2>
+        <div className="producto-item">
+          <img src={paduno} alt="Producto Pad" className="pad1" />
+          <div className="comprar">
+            <button className="boton1" onClick={handleCompraClick}>
+              <Link to="/carrito" className="boton">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                Añadir producto
+              </Link>
+            </button>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
